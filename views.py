69fdf8c2a06cd93daea8690f2ec3fefb10e050b5
Freddy Mercado <frmercado@emcali.com.co>
2024-10-03 12:28:12 -0500

    03102024-1227

--- views.py
+++ views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 import datetime
 from django.template import Template, Context
-
+from django.template import loader
 
 class Persona(object):
 
@@ -13,20 +13,21 @@ class Persona(object):
         
 def saludo(request):
 
-    p1=Persona("Profesor Jair", "Jirafales")
+    p1=Persona("Jair", "Jirafales")
     
     #nombre="Freddy"
     #apellido="Mercado"
 
-    temas2=[]
+    temas2=["Lista1, Plista2, Clista3, Dlista4, Slista5"]
     temas3=[]
     ahora=datetime.datetime.now()
-    doc_externo=open("D:/ProyectoDjango/Proyecto1/Proyecto1/Plantillas/plantilla1.html")
-    plt=Template(doc_externo.read())
-    doc_externo.close()
-    ctx=Context({"nombre_persona":p1.nombre, "apellido_persona":p1.apellido, "momento_actual":ahora,"temas":temas2}) #Asiganacion de una clave a un valor alamacenado ne una variable
+    doc_externo=loader.get_template('plantilla1.html')
+    #doc_externo=open("D:/ProyectoDjango/Proyecto1/Proyecto1/Plantillas/plantilla1.html")
+    #plt=Template(doc_externo.read())
+    #doc_externo.close()
+    #ctx=Context({"nombre_persona":p1.nombre, "apellido_persona":p1.apellido, "momento_actual":ahora,"temas":temas2}) #Asiganacion de una clave a un valor alamacenado ne una variable
     
-    documento=plt.render(ctx)
+    documento=doc_externo.render({"nombre_persona":p1.nombre, "apellido_persona":p1.apellido, "momento_actual":ahora,"temas":temas2})
 
     return HttpResponse (documento)
 
